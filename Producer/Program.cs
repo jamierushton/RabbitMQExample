524a7c30556bfe7437e52e3bab1453e5a57f854d
Jamie <jamierushton@users.noreply.github.com>
2017-10-01 15:56:51 +0100

    Added Rabbit code

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace Producer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("hello", false, false, false, null);
+
+                var acceptingMessages = true;
+                while (acceptingMessages)
+                {
+                    var message = Console.ReadLine() ?? string.Empty;
+                    if (string.Equals(message, "exit", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        acceptingMessages = false;
+                    }
+                    else
+                    {
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(string.Empty, "hello", null, body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                }
+            }
         }
     }
 }
